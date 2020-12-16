92554a146c287c5849251a8b870300e34eff05a0
zy-777 <476858387@qq.com>
2020-12-16 10:06:52 +0800

    new

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,15 +1,40 @@
 <template>
   <div id="content">
-
+    <div class="eachOne" v-for="item in list" :key='item.id'>
+      <div class="title">{{ item.title }}</div>
+      <div class="name">
+        <span>{{ item.name }}</span>
+        <span>{{item.date}}</span>
+      </div>
+      <div class='text'>
+        {{item.text}}
+      </div>
+      <div>
+        <div class="left">
+          <span>公司注冊</span>
+          <span>繳納</span>
+        </div>
+        <div class="right">
+          <span>+</span>
+          <span>-</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-name:'Content'
-}
+  name: "Content"
+};
 </script>
 
-<style>
-
+<style lang="scss">
+#content{
+  width: 100%;
+  >.eachOne{
+    width: 100%;
+    background: #fff;
+  }
+}
 </style>
