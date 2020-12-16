e96c34df77fcead07a9d110facf26b4780fdb3a8
LWJ <1397336533@qq.com>
2020-12-16 08:57:29 +0800

    1

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App.vue'
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+import router from './router'
+
+Vue.config.productionTip = false
+Vue.use(MintUI)
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
